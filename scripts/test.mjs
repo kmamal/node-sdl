243b2c3aa98d71f11ac88231d3050f9d59b50b76
Liplum <Li_plum@outlook.com>
2023-03-03 16:06:07 +0800

    fixed "__dirname" in scripts/test.mjs

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -1,8 +1,10 @@
 import * as Path from 'path'
 import { execSync } from 'child_process'
 import { fstat } from 'fs'
+import { fileURLToPath } from 'url'
+const __dirname = Path.dirname(fileURLToPath(import.meta.url))
 
-process.chdir(Path.resolve(__dirname, '../tests'))
+process.chdir(Path.join(__dirname, '../tests'))
 const files = await fstat.readdir('.')
 for (const name of files) {
 	if (!name.endsWith('.test.js')) { continue }
