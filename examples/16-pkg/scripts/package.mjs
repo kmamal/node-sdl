9ec72cb248de0c4f6d373fe5c94ef537ae127354
KM <the.real.laplace@gmail.com>
2024-02-07 03:21:35 +0000

    fix example

--- examples/16-pkg/scripts/package.mjs
+++ examples/16-pkg/scripts/package.mjs
@@ -5,7 +5,11 @@ import fs from 'node:fs'
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
-execSync('npx pkg --output dist/example .', {
+const majorVersion = process.version.slice(0, process.version.indexOf('.'))
+const target = `node${majorVersion}-${process.platform}-${process.arch}`
+const ext = process.platform === 'win32' ? '.exe' : ''
+
+execSync(`npx pkg --target ${target} --output dist/example${ext} .`, {
 	cwd: path.resolve(__dirname, '..'),
 	stdio: 'inherit',
 })
