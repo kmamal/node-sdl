b9013b10566b7555568387b5a76e8c6bccb4ea52
KM <the.real.laplace@gmail.com>
2026-08-17 13:31:44 +0300

    wrap global fetch

--- scripts/util/fetch.js
+++ scripts/util/fetch.js
@@ -1,84 +1,33 @@
+const Stream = require('stream')
 
-const _consume = async (stream) => {
-	const chunks = []
-	for await (const chunk of stream) {
-		chunks.push(chunk)
-	}
-	return Buffer.concat(chunks)
-}
+const fetch = async (url, options = {}) => {
+	const response = await globalThis.fetch(url, options)
 
-const fetch = async (_url, options = {}) => {
-	let url = _url
-	let {
-		maxRedirect = 5,
-		body = null,
-	} = options
+	const { status: statusCode } = response
 
-	const { protocol } = new URL(url)
-	const lib = require(protocol.slice(0, -1))
-
-	const evalPromise = (resolve, reject) => {
-		const request = lib.request(url, options)
-			.on('error', reject)
-			.on('response', resolve)
-
-		if (!body) {
-			request.end()
-			return
-		}
-
-		if (body?.length) {
-			if (!Buffer.isBuffer(body)) { body = Buffer.from(body) }
-			body = Promise.resolve(body)
-		}
-		else {
-			body = _consume(body)
-		}
-
-		body.then((buffer) => {
-			request.setHeader('Content-Length', buffer.length)
-			request.end(buffer)
+	if (!response.ok) {
+		let responseBody
+		try { responseBody = await response.text() }
+		catch (_) {}
+		try { responseBody = JSON.parse(responseBody) }
+		catch (_) {}
+		throw Object.assign(new Error(`bad status code ${statusCode}`), {
+			statusCode,
+			responseBody,
 		})
 	}
 
-	for (;;) {
-		const response = await new Promise(evalPromise)
-
-		const { statusCode } = response
-
-		if (300 <= statusCode && statusCode < 400) {
-			if (maxRedirect-- > 0) {
-				url = response.headers.location
-				continue
-			}
-		}
-
-		if (!(200 <= statusCode && statusCode < 300)) {
-			let responseBody
-			try { responseBody = (await _consume(response)).toString() }
-			catch (_) {}
-			try { responseBody = JSON.parse(responseBody) }
-			catch (_) {}
-			throw Object.assign(new Error(`bad status code ${statusCode}`), {
-				statusCode,
-				responseBody,
-			})
-		}
-
-		return {
-			stream () { return response },
-			async buffer () {
-				return await _consume(response)
-			},
-			async text () {
-				const buffer = await this.buffer()
-				return buffer.toString()
-			},
-			async json () {
-				const text = await this.text()
-				return JSON.parse(text)
-			},
-		}
+	return {
+		stream () { return Stream.Readable.fromWeb(response.body) },
+		async buffer () {
+			return Buffer.from(await response.arrayBuffer())
+		},
+		async text () {
+			return await response.text()
+		},
+		async json () {
+			return await response.json()
+		},
 	}
 }
 
